93168295617bfa2205fe1c8fea0e1eee0d56b455
Priyabrata Sarkar <yespbs@gmail.com>
2015-07-29 19:05:55 +0530

    update

---
+++ application/controllers/api/Dashboard.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH . '/libraries/REST_Controller.php';
+
+
+class Dashboard extends REST_Controller {
+
+	public function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        $this->methods['user_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['user_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['user_delete']['limit'] = 50; // 50 requests per hour per user/key
+    }
+
+    public function index()
+    {
+        
+    }
+}    
