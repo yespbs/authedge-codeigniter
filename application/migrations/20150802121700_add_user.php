df9d032296e08b654aae5efec8d0ee8313eef0f2
Priyabrata Sarkar <yespbs@gmail.com>
2015-08-02 00:25:43 +0530

    add migration

---
+++ application/migrations/20150802121700_add_user.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_user extends CI_Migration {
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 10,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'password' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '60',
+            ),
+            'remember_token' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'null' => true,
+            ),
+            'created_at' => array(
+                'type' => 'TIMESTAMP',                
+            ),
+            'updated_at' => array(
+                'type' => 'TIMESTAMP',                
+            ),
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('users');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('users');
+    }
+}
