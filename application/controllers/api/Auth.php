3367ac77e5e5a0d0f0335062d85fe46a84235983
Priyabrata Sarkar <yespbs@gmail.com>
2015-08-02 10:18:07 +0530

    add html

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -13,10 +13,28 @@ class Auth extends REST_Controller {
         parent::__construct();
         // Configure limits on our controller methods
         // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-        $this->methods['user_get']['limit'] = 500; // 500 requests per hour per user/key
-        $this->methods['user_post']['limit'] = 100; // 100 requests per hour per user/key
-        $this->methods['user_delete']['limit'] = 50; // 50 requests per hour per user/key
+        $this->methods['login_post']['limit'] = 500; // 500 requests per hour per user/key
+       // $this->methods['user_get']['limit'] = 500; // 500 requests per hour per user/key
+       // $this->methods['user_post']['limit'] = 100; // 100 requests per hour per user/key
+       // $this->methods['user_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
+
+    public function login_post()
+    {
+        $user = ['id' => 1, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed on CodeIgniter'];
+
+        if (!empty($user))
+        {
+            $this->set_response($user, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+        }
+        else
+        {
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'User could not be found'
+            ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        }
+    }    
 
     public function users_get()
     {
@@ -66,6 +84,7 @@ class Auth extends REST_Controller {
                 }
             }
         }
+        
         if (!empty($user))
         {
             $this->set_response($user, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
---
+++ public/assets/js/authedge.js
@@ -0,0 +1,45 @@
+var AuthEdge = angular.module('AuthEdge', ['ngRoute']);
+
+
+AuthEdge.controller('AuthController', function( $scope ){
+	$scope.url = '/api/login';
+
+	/*login: function(){
+
+	}*/
+});
+
+AuthEdge.config( function( $routeProvider ){
+	$routeProvider
+	.when('/',
+		{
+			controller: 'AuthController',
+			templateUrl: 'assets/html/login.html'
+		}
+	)
+	.when('/forgotpass',
+		{
+			controller: 'AuthController',
+			templateUrl: 'assets/html/forgotpass.html'
+		}
+	)
+	.when('/resetpass',
+		{
+			controller: 'AuthController',
+			templateUrl: 'assets/html/resetpass.html'
+		}
+	)
+	.when('/signup',
+		{
+			controller: 'AuthController',
+			templateUrl: 'assets/html/signup.html'
+		}
+	)
+	.when('/dashboard',
+		{
+			controller: 'AuthController',
+			templateUrl: 'assets/html/dashboard.html'
+		}
+	)
+	.otherwise( {redirectTo: '/'} );
+} );
